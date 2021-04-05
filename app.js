6319dc6c2efa993cc961e6e74247467176c56a40
deje0014 <72152264+deje0014@users.noreply.github.com>
2021-04-05 17:56:22 -0400

    Removed Debug dependencie and added Winston. Middleware function created for it. Added Config and ConnectDatabase.js updated to use variables.

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
 // Don't forget to use NPM to install Express and Mongoose.
 import morgan from 'morgan'
+import log from './startup/logger.js'
 import express from 'express'
+import compression from 'compression'
+import cors from 'cors'
 import sanitizeMongo from 'express-mongo-sanitize'
-import authRouter from './routes/auth/index.js'
 import connectDatabase from './startup/connectDatabase.js'
 connectDatabase()
 
 
 const app = express()
 
+log.info(process.env.NODE_ENV)
+log.warn(app.get('env')) //if NODE_ENV is undefined, returns development
+
+app.use(cors())
+app.use(helmet())
+app.use(compression())
 app.use(morgan('tiny'))
+app.use(cors())
+app.use(express.static('public'))
 app.use(express.json())
 app.use(sanitizeMongo())
 
 // routes
 app.use('/auth', authRouter)
+
+
+// error handler middleware
+//to be created
 
 
 export default app
