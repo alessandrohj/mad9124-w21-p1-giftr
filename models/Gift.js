a481f78801095af6e9004c32c5b7ee39c67c69d0
GYUYOUNG LEE <lee00777@algonquinlive.com>
2021-04-10 16:07:47 +0900

    Set minlength and maxlength for String type and min and max for Number type in Gift schema

--- models/Gift.js
+++ models/Gift.js
@@ -2,30 +2,30 @@ import mongoose from 'mongoose'
 
 const schema = new mongoose.Schema({
   name: {
-      type: String,
-      required: true,
-      minLength: 4,
-      maxLength: 64,
-      required: true
-  }, 
+    type: String,
+    required: true,
+    minlength: 4,
+    maxlength: 64,
+    required: true,
+  },
   price: {
-      type: Number,
-      min: 100,
-      default: 1000
+    type: Number,
+    min: 100,
+    default: 1000,
   },
   imageUrl: {
-      type: String,
-      min: 1024
+    type: String,
+    min: 1024,
   },
   store: {
-      name: {
-          type: String,
-          max: 254
-      },
-      productURL: {
-          type: String,
-          max: 1024
-      }
+    name: {
+      type: String,
+      maxlength: 254,
+    },
+    productURL: {
+      type: String,
+      maxlength: 1024,
+    },
   },
 })
 const Model = mongoose.model('Gift', schema)
