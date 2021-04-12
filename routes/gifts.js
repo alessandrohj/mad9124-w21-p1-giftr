5faf863cc1c0898f2c140393cd7414f238d5ec5e
GYUYOUNG LEE <lee00777@algonquinlive.com>
2021-04-12 11:54:35 +0900

    Implement delete router logic for an array of git sub documents in Gift router

--- routes/gifts.js
+++ routes/gifts.js
@@ -15,11 +15,9 @@ router.post('/:id/gifts', authUser, sanitizeBody, async (req, res) => {
   let newDocument = new Gift(req.sanitizedBody)
   try {
     await newDocument.save()
-
     const savedDocument = await Person.findOne({ _id: req.params.id })
     savedDocument.gifts.push(newDocument._id)
     await savedDocument.save()
-
     res.status(201).send({ data: newDocument })
   } catch (err) {
     log(err)
@@ -41,6 +39,7 @@ const update = (overwrite = false) => async (req, res) => {
     const savedDocument = await Person.findById(req.params.id)
     const savedGiftDocument = await savedDocument.gifts
     savedGiftDocument.forEach((gift) => {
+      console.log(gift)
       if (gift == req.params.giftId) {
         gift = req.sanitizedBody
       }
@@ -57,11 +56,15 @@ router.patch('/:id/gifts/:giftId', authUser, sanitizeBody, update(false))
 router.delete('/:id/gifts/:giftId', authUser, async (req, res) => {
   try {
     const document = await Gift.findByIdAndRemove(req.params.giftId)
+    const savedDocument = await Person.findOne({ _id: req.params.id })
+    const savedGiftDocument = await savedDocument.gifts
+    const idx = savedGiftDocument.indexOf(req.params.giftId)
+    savedDocument.gifts.splice(idx, 1)
+    await savedDocument.save()
     if (!document) throw new ResourceNotFoundException('Resource not found')
     res.send({ data: document })
   } catch (err) {
     handleErrors(req, res)
   }
 })
-
 export default router
